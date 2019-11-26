179f06a8eb063587f052db192c6e99d094cd75b1
Andrei <giugleaandrei@ymail.com>
2019-11-27 00:07:02 +0200

    Initial Commit

---
+++ TransferRapidSwifty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransferRapidSwifty
+//
+//  Created by Andrei Giuglea on 27/11/2019.
+//  Copyright © 2019 Andrei Giuglea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
